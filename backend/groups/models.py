4af5a4d4362d15718020a896004570778c461df5
Sriram Sendhil <sriramsendhil@gmail.com>
2025-04-18 00:20:56 -0500

    Restore API-related group files from commit 8a1abab

--- backend/groups/models.py
+++ backend/groups/models.py
@@ -5,6 +5,7 @@ class Class(models.Model):
     name = models.CharField(max_length=255)
     teacher = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='classes')
     created_at = models.DateTimeField(auto_now_add=True)
+    students = models.ManyToManyField(CustomUser, related_name='enrolled_classes', blank=True)
 
     def __str__(self):
         return self.name
