0d0efc39ce1864bd886906a43f06a328f304e78f
Sriram Sendhil <sriramsendhil@gmail.com>
2025-04-24 17:55:30 -0500

    created view for login / sign up fixed issues with django account sessions history

--- seniordesign_project/services/user_mutations.ts
+++ seniordesign_project/services/user_mutations.ts
@@ -17,3 +17,33 @@ export const LOGOUT_MUTATION = gql`
     logout
   }
 `;
+
+export const CREATE_USER_MUTATION = gql`
+  mutation RegisterUser($name: String!, $email: String!, $password: String!, $role: String!) {
+    register_user(name: $name, email: $email, password: $password, role: $role) {
+      user_id
+      name
+      email
+      role
+      created_at
+    }
+  }
+`
+
+  export const CREATE_ASSIGNMENT = gql`
+    mutation CreateAssignment(
+      $classId: Int!
+      $name: String!
+      $dueDate: DateTime
+      $prompt: String
+    ) {
+      create_assignment(
+        class_id: $classId
+        name: $name
+        due_date: $dueDate
+        prompt: $prompt
+      )
+    }
+`;
+
+
--- seniordesign_project/services/user_queries.ts
+++ seniordesign_project/services/user_queries.ts
@@ -24,3 +24,5 @@ export const GET_TEACHER_CLASSES = gql`
     }
   }
 `;
+
+
